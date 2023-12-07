c6b1dd8ac23ab578551428d7d90cb4326e1d2e1a
Yehor Makarenko <mak.yehor.knu@gmail.com>
2023-12-07 14:38:25 +0200

    Implemented round robin without i/o blocking time

--- scheduler/SchedulingAlgorithm.java
+++ scheduler/SchedulingAlgorithm.java
@@ -2,10 +2,9 @@
 // the scheduling algorithm written by the user resides.
 // User modification should occur within the Run() function.
 
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Vector;
-import java.util.Map.Entry;
 import java.io.*;
 
 public class SchedulingAlgorithm {
@@ -13,70 +12,68 @@ public class SchedulingAlgorithm {
   public static Results Run(int runtime, Vector processVector, Results result) {
     int i = 0;
     int comptime = 0;
-    int currentProcess = 0;
-    int previousProcess = 0;
+    int currentProcess = 0;    
     int size = processVector.size();
     int completed = 0;
+    int quantum = 200;
+    int quantumTime = 0;
     String resultsFile = "Summary-Processes";
+    Queue<Integer> processQueue = new LinkedList<>();
 
     result.schedulingType = "Batch (Nonpreemptive)";
     result.schedulingName = "First-Come First-Served";         
 
     try {
-      //BufferedWriter out = new BufferedWriter(new FileWriter(resultsFile));
-      //OutputStream out = new FileOutputStream(resultsFile);
-      PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
+      PrintStream out = new PrintStream(new FileOutputStream(resultsFile));            
 
-
-      TreeMap<Integer, sProcess> tm = new TreeMap<>();
-      for (int j = size - 1; j >= 0; j--) {
-        sProcess process = (sProcess) processVector.elementAt(currentProcess);
-        tm.put(j, process);
+      for (int j = 0; j < size; j++) { 
+        processQueue.add(j);
       }
-      for (Entry<Integer, sProcess> p: tm.entrySet()) {
-        out.println("Process: " + p.getKey() + " registered... (" + p.getValue().cputime + " " + p.getValue().ioblocking + " " + p.getValue().cpudone + " " + p.getValue().cpudone + ")");
-      }
-      out.print("\n\n\n");
 
+      sProcess process = (sProcess) processVector.get(processQueue.poll());
+      out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
 
-      sProcess process = (sProcess) processVector.elementAt(currentProcess);
-      out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
       while (comptime < runtime) {
         if (process.cpudone == process.cputime) {
           completed++;
-          out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+          out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
           if (completed == size) {
             result.compuTime = comptime;
             out.close();
             return result;
           }
-          for (i = size - 1; i >= 0; i--) {
-            process = (sProcess) processVector.elementAt(i);
-            if (process.cpudone < process.cputime) { 
-              currentProcess = i;
-            }
-          }
-          process = (sProcess) processVector.elementAt(currentProcess);
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+
+          currentProcess = processQueue.poll();
+          process = (sProcess) processVector.get(currentProcess);          
+          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
+          quantumTime = 0;
         }      
-        if (process.ioblocking == process.ionext) {
-          out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+
+        while (process.ioblocking == process.ionext) {
+          out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
           process.numblocked++;
-          process.ionext = 0; 
-          previousProcess = currentProcess;
-          for (i = size - 1; i >= 0; i--) {
-            process = (sProcess) processVector.elementAt(i);
-            if (process.cpudone < process.cputime && previousProcess != i) { 
-              currentProcess = i;
-            }
-          }
-          process = (sProcess) processVector.elementAt(currentProcess);
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
-        }        
+          process.ionext = 0;           
+          processQueue.add(currentProcess);
+          currentProcess = processQueue.poll();
+          process = (sProcess) processVector.get(currentProcess);
+          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
+          quantumTime = 0;
+        }    
+        
+        if (quantumTime == quantum) {        
+          out.println("Process: " + currentProcess + " interrupted... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
+          processQueue.add(currentProcess);
+          currentProcess = processQueue.poll();
+          process = (sProcess) processVector.get(currentProcess);
+          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
+          quantumTime = 0;
+        }
+
         process.cpudone++;       
         if (process.ioblocking > 0) {
           process.ionext++;
         }
+        quantumTime++;
         comptime++;
       }
       out.close();
